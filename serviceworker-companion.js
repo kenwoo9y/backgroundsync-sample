55d3f6813a46f8546b539c4738b08e8917fd1cec
kenwoo9y <ei.1px.9.01@gmail.com>
2020-05-05 18:39:07 +0900

    add initializeServiceWorker

--- serviceworker-companion.js
+++ serviceworker-companion.js
@@ -1,16 +1,37 @@
-// register service worker
+// get element
+const form = document.getElementById('form');
 
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/backgroundsync-sample/serviceworker.js', { scope: '/backgroundsync-sample/' })
-    .then(function(registration){
-        if (registration.installing) {
-            console.log('Service Worker installing');
-        } else if (registration.waiting) {
-            console.log('Service Worker installed');
-        } else if (registration.active) {
-            console.log('Service Worker active');
-        }
-    }).catch(function(error){
-        console.log('Registration failed with ' + error);
-    })
+function initializeServiceWorker() {
+    // register service worker
+    if (navigator.serviceWorker) {
+        navigator.serviceWorker.register('/backgroundsync-sample/serviceworker.js', { scope: '/backgroundsync-sample/' })
+        .then(function() {
+            return navigator.serviceWorker.ready;
+        })
+        .then(function(registration) {
+            form.addEventListener('submit', function(event) {
+                event.preventDefault();
+                saveData().then(function() {
+                    if(registration.sync) {
+                        registration.sync.register('example-sync')
+                        .catch(function(error) {
+                            return error;
+                        })
+                    } else {
+                        checkInternet();
+                    }
+                });
+            });
+        })
+        .catch(function(error) {
+            console.log('Registration failed with ' + error);
+        })
+    } else {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            saveData().then(function() {
+                checkInternet();
+            });
+        })
+    }
 }
