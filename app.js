7be38af42133fb3463666bd232fa3bb830909883
kenwoo9y <ei.1px.9.01@gmail.com>
2020-07-06 23:44:54 +0900

    update sendData

--- app.js
+++ app.js
@@ -146,16 +146,14 @@ function clearData() {
 
 function sendData() {
     fetchData().then(function(response) {
-        let sendItem = {
+        // unfinished
+        return window.fetch('', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(response)
-        };
-
-        // unfinished
-        return window.fetch('', sendItem);
+        });
     })
     .then(clearData)
     .catch(function(error) {
